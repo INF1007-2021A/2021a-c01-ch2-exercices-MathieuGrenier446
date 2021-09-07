5e41a014ec5b77e9a0c0dfbe01ae094a64ccfa63
MathieuGrenier446 <90001228+MathieuGrenier446@users.noreply.github.com>
2021-09-07 17:30:49 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-def majuscule(mot):
-    # TODO completer la fonction ici
-    return mot
 
+def majuscule(mot):
+    mots=""
+    for i in mot:
+        mots+=chr(ord(i)-32)
+    return mots
 
 if __name__ == '__main__':
     mots = [
